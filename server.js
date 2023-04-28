94c1cd62859865331643fd1917625f4c01fbd697
krunal04111993 <107112713+krunal04111993@users.noreply.github.com>
2023-04-28 18:02:52 +0530

    Update server.js
    cd

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const auth = (req,res,  next) => {
 	next();
 }
 app.get('/',  (req, res) => {
-	res.send('Hello World brother jenkins!');
+	res.send('Hello World brother jenkins world!');
   })
 app.get('/testgetrequest',  (req, res) => {
 	res.send('Hello World!');
